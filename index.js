a09967fd156b9282248b5b32909867ceb63807d4
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-10 22:03:15 -0400

    set up index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+// 1 - bring in express, routers, and any other imports
+const express = require("express");
+const userRouter = require("./users/userRouter");
+
+// 2 - create server
+const server = express();
+
+// 3 - add middleware, including routers
+server.use(express.json());
+server.use(userRouter);
+
+// 4 - have server listen
+server.listen(8000, () => {
+    console.log("Server running on http://localhost:8000")
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,30 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+// Create User
 router.post('/', (req, res) => {
   // do your magic!
 });
 
+// Create Post
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// Get Users
 router.get('/', (req, res) => {
   // do your magic!
 });
 
+// Get User By ID
 router.get('/:id', (req, res) => {
   // do your magic!
 });
 
+// Get Posts By User ID
 router.get('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// Delete User
 router.delete('/:id', (req, res) => {
   // do your magic!
 });
 
+// Update User
 router.put('/:id', (req, res) => {
   // do your magic!
 });
